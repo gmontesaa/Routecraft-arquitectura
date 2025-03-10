a3fe4398e5f7746fb84d7bcaf8f980adf0ebf6cb
juanmaDsrt <jmflorezrobledo@gmail.com>
2025-03-10 10:17:43 -0500

    primer commit

---
+++ places/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    #return HttpResponse(' <h1>Welcome to home page</h1>')
+    #return render(request,'home.html')
+    return render(request,'home.html', {'name':'Juan Manuel'})
+
+
+def about(request):
+    return render(request, 'about.html')
